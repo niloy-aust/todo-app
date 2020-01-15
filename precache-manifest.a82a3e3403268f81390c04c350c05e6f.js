8167bb8d738e0c263f9ce6da0ed2fd748296a86d
Niloy Saha <niloyaust012@gmail.com>
2020-01-15 22:24:49 +0600

    Updates

---
+++ precache-manifest.a82a3e3403268f81390c04c350c05e6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1aba90d81ecc326642297a277f8b62",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "4f6e622bf0265ab9b632",
+    "url": "/todo-app/static/css/2.48c08b49.chunk.css"
+  },
+  {
+    "revision": "90287ec864f4380c6bc2",
+    "url": "/todo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4f6e622bf0265ab9b632",
+    "url": "/todo-app/static/js/2.052e27fd.chunk.js"
+  },
+  {
+    "revision": "18db333adb41a97042cb1281e6b75be4",
+    "url": "/todo-app/static/js/2.052e27fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90287ec864f4380c6bc2",
+    "url": "/todo-app/static/js/main.9c71f14c.chunk.js"
+  },
+  {
+    "revision": "8593387752c70bbafc80",
+    "url": "/todo-app/static/js/runtime-main.f7fdef70.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
